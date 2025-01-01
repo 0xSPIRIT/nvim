728cf9ba51f33d0a242625ca8aaf608cecfac31a
Ameer <ameergamez06@gmail.com>
2025-01-01 19:29:54 -0400

    Initial commit

---
+++ config.vim
@@ -0,0 +1,62 @@
+command W w
+
+let g:neovide_cursor_animation_length = 0.0
+let g:neovide_scroll_animation_length = 0.07
+
+"colorscheme dosbox-black
+"colorscheme fahrenheit
+"colorscheme ashen
+"set guifont=Cascadia\ Code:h12
+set guifont=Cascadia\ Code:h12
+"set guifont=Liberation\ Mono:h12
+"set guifont=Lucida\ Console:h14
+"set guifont=Perfect\ DOS\ VGA\ 437\ Win:h19
+
+set wrap
+set lbr
+set clipboard=unnamedplus
+set expandtab
+set smartindent
+set relativenumber
+set autoindent
+"set cursorline
+set cindent
+set tabstop=4
+set shiftwidth=4
+set cinoptions=(0,l1
+set ignorecase
+set smartcase
+set guicursor=n-v-c-i:block
+set nohlsearch
+let g:neovide_fullscreen=v:true
+
+autocmd bufreadpre *.txt setlocal textwidth=64
+"set textwidth=64
+
+set omnifunc=complete#Complete
+set tags=c:/dev/system_tags/system.tags,tags
+
+noremap K k
+
+imap <C-c> <ESC>
+
+map <C-p> :vs<CR>
+map <C-S-p> :q<CR>
+
+"noremap <C-o> :call feedkeys(":e ")<CR>
+"noremap <Space> :call feedkeys(":b ")<CR>
+
+noremap <C-e> zz
+nnoremap <C-l> <C-w><C-w>
+nnoremap gd <C-]>
+
+"noremap <C-s> :call feedkeys(":grep ")<CR>
+
+noremap <F9> :e $MYVIMRC<CR>
+noremap <C-F9> :e ~/AppData/Local/nvim/config.vim<CR>
+noremap <F12> :source $MYVIMRC<CR>
+noremap <C-Enter> :!tag<CR>
+
+noremap <C-Space> :call feedkeys(":tag ")<CR>
+
+noremap <A-m> :!build<CR>
---
+++ init.lua
@@ -0,0 +1,113 @@
+vim.cmd('source ~/AppData/Local/nvim/config.vim')
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        'nvim-telescope/telescope-project.nvim',
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+    },
+    {
+        'ahmedkhalf/project.nvim',
+        config = function()
+            require('project_nvim').setup({
+                detection_methods = { "pattern", "git" },  -- Methods for detecting projects
+                silent_chdir = true,  -- Set to true if you don’t want auto cd to project dir
+            })
+        end
+    },
+    {
+        "ficcdaf/ashen.nvim",
+        lazy = false,
+        priority = 1000,
+    }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = false },
+})
+
+
+local actions = require('telescope.actions')
+
+require('telescope').setup({
+    defaults = {
+        vimgrep_arguments = {
+            'rg',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+        },
+        pickers = {
+            find_files = {
+                find_command = {'fd', '--type', 'f'},
+            },
+            git_files = {hidden = true},  -- Disable git_files
+            git_status = {hidden = true}, -- Disable git_status
+            git_branches = {hidden = true}, -- Disable git_branches
+        },
+        mappings = {
+            i = {
+                ["<C-c>"] = actions.close,
+                ["<ESC>"] = actions.close,
+            },
+            n = {
+                ["<C-c>"] = actions.close,
+                ["<ESC>"] = actions.close,
+            },
+        },
+    },
+})
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<C-o>', builtin.find_files, { desc = 'Telescope find files' })
+
+vim.keymap.set('n', '<C-s>', builtin.live_grep, { desc = 'Telescope live grep' })
+
+vim.keymap.set('n', '<Space>', function()
+    builtin.buffers({
+        sort_lastused = true,
+    })
+end, { desc = 'Telescope buffers' })
+
+vim.cmd('colorscheme ashen')
